712332d29e766eb05afc761bde1090f2b1af8513
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-12-31 03:56:38 -0500

    Clean up/change test uuid

--- src/test/java/com/azuredoom/levelingcoretesting/LevelServiceTest.java
+++ src/test/java/com/azuredoom/levelingcoretesting/LevelServiceTest.java
@@ -10,19 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LevelServiceTest {
 
-    public static final System.Logger LOGGER = System.getLogger(Main.class.getName());
-
     @Test
     void addXp_increasesXpAndLevel() {
         var service = AppBootstrap.createLevelService(Main.configPath);
-        var id = UUID.fromString("d3804858-4bb8-4026-ae21-386255ed467d");
+        var id = UUID.fromString("00000000-0000-0000-0000-000000000001");
         var amount = 500;
         var beforeXp = service.getXp(id);
         var beforeLevel = service.getLevel(id);
 
         service.addXp(id, amount);
-        LOGGER.log(System.Logger.Level.INFO, String.format("XP: %d", service.getXp(id)));
-        LOGGER.log(System.Logger.Level.INFO, String.format("Level: %d", service.getLevel(id)));
+        Main.LOGGER.log(System.Logger.Level.INFO, String.format("XP: %d", service.getXp(id)));
+        Main.LOGGER.log(System.Logger.Level.INFO, String.format("Level: %d", service.getLevel(id)));
 
         assertEquals(beforeXp + amount, service.getXp(id));
         assertTrue(service.getLevel(id) >= beforeLevel);
